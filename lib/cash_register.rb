d70e12ccbc94d286f630374173cfc9a0bca7180b
Tyler Carroll <tylercarroll1611@gmail.com>
2019-11-04 21:16:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,6 +20,12 @@ class CashRegister
       @discount = @discount/100.to_f
       @total = @total - (@total * (@discount))
       return "After the discount, the total comes to $#{total.to_i}."
+    else
+      return "There is no discount to apply."
+    end 
   end 
   
+  def void_last_transaction
+    self.total = self.total - self.last_transaction
+  end 
 end 
